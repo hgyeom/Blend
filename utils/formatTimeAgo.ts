f8518c0514c47874e83f5016dff4a4f7fecdd714
hgyeom <rrune3@naver.com>
2024-07-29 17:06:15 +0900

    feat : 메인 페이지 무한 스크롤 기능 및 작성일 계산 함수 변경
    MainPage.tsx
    무한 스크롤 구현
    useQuery > useInfiniteQuery 변경
    테스트 함수 삭제
    
    lib/api/posts.ts
    client side에서 server side로 변경
    테스트 함수삭제 및 사용 함수 변경
    
    utils/formatTiemAgo.ts
    작성일 계산하는 로직 변경

--- utils/formatTimeAgo.ts
+++ utils/formatTimeAgo.ts
@@ -1,43 +1,50 @@
 const formatTimeAgo = (dateString: string): string => {
+  // 주어진 날짜 문자열을 Date 객체로 변환
   const createdDate = new Date(dateString)
 
+  // 현재 시간에 9시간을 더한 Date 객체 생성 (kst 기준)
   const nowDate = new Date(new Date().getTime() + 9 * 60 * 60 * 1000)
 
-  // 주어진 날짜와 현재 날짜의 차이를 계산 (밀리초 단위)
+  // 주어진 날짜와 현재 날짜의 차이를 밀리초 단위로 계산
   const timeDifference = nowDate.getTime() - createdDate.getTime()
 
-  const cur = new Date('2023/01/10 09:00:00')
-  const pre = new Date('2023/01/10 06:00:00')
-  const diff = cur.getTime() - pre.getTime()
+  const getDifferenceString = (value: number, unit: string) => `${Math.floor(value)}${unit} 전`
 
-  // 밀리초를 다양한 시간 단위로 변환
-  const minutesDifference = Math.floor(timeDifference / (60 * 1000))
-  const hoursDifference = Math.floor(timeDifference / (60 * 60 * 1000))
-  const daysDifference = Math.floor(timeDifference / (60 * 60 * 1000 * 24))
-  const monthsDifference = Math.floor(diff / (60 * 60 * 1000 * 24 * 30)) // 평균 30일
-  const yearsDifference = Math.floor(diff / (60 * 60 * 1000 * 24 * 365)) // 평균 365일
+  // 밀리초를 초 단위로 변환
+  const seconds = timeDifference / 1000
 
-  if (yearsDifference > 0) {
-    return `${yearsDifference}년 전`
-  }
-  if (monthsDifference > 0) {
-    return `${monthsDifference}개월 전`
-  }
-  if (daysDifference > 1) {
-    // 1일 이상인 경우
-    return `${daysDifference}일 전`
-  }
-  if (daysDifference === 1) {
-    // 1일인 경우
-    return '어제'
-  }
-  if (hoursDifference > 0) {
-    return `${hoursDifference}시간 전`
-  }
-  if (minutesDifference > 0) {
-    return `${minutesDifference}분 전`
-  }
-  return '방금 전'
+  // 초 단위 차이가 60보다 작으면 '방금 전' 반환
+  if (seconds < 60) return `방금 전`
+
+  // 초 단위를 분 단위로 변환
+  // 분 차이가 60보다 작으면 몇 분 전인지 반환
+  const minutes = seconds / 60
+  if (minutes < 60) return getDifferenceString(minutes, '분')
+
+  // 분 단위를 시간 단위로 변환
+  // 시간 차이가 24보다 작으면 몇 시간 전인지 반환
+  const hours = minutes / 60
+  if (hours < 24) return getDifferenceString(hours, '시간')
+
+  // 시간 단위를 일 단위로 변환
+  // 일 차이가 7보다 작으면 몇 일 전인지 반환
+  const days = hours / 24
+  if (days < 7) return getDifferenceString(days, '일')
+
+  // 일 단위를 주 단위로 변환
+  // 주 차이가 5보다 작으면 몇 주 전인지 반환
+  const weeks = days / 7
+  if (weeks < 5) return getDifferenceString(weeks, '주')
+
+  // 일 단위를 월 단위로 변환 (평균 30일로 계산)
+  // 월 차이가 12보다 작으면 몇 개월 전인지 반환
+  const months = days / 30
+  if (months < 12) return getDifferenceString(months, '개월')
+
+  // 일 단위를 년 단위로 변환 (평균 365일로 계산)
+  // 몇 년 전인지 반환
+  const years = days / 365
+  return getDifferenceString(years, '년')
 }
 
 export default formatTimeAgo
